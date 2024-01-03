9cf4a1b68705c087c67d041302943da95cea078a
somnico <54187491+somnico@users.noreply.github.com>
2024-01-03 15:47:44 +0100

    Update kali.bat

--- connect/kali.bat
+++ connect/kali.bat
@@ -1,5 +1,5 @@
 @echo off
-start /B cmd /c "ssh -L 5901:localhost:5901 -N -f -i  C:\Users\nfs\kali.pem kali@16.16.201.235"
+start /B cmd /c "ssh -o StrictHostKeyChecking=no -L 5901:localhost:5901 -N -f -i kali.pem kali@13.51.121.180"
 start "" "C:\Program Files\RealVNC\VNC Viewer\vncviewer.exe"
 "C:\Program Files\AutoHotkey\AutoHotKey.exe" "C:\Users\nfs\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\kali.ahk"
 taskkill /F /IM cmd.exe
